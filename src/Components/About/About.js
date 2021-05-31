6d323767eb5b6c468db0f28932c65f1298a39f23
strigonone <s15jaychao@hotmail.com>
2021-06-01 01:37:43 +1200

    Created About section draft

---
+++ src/Components/About/About.js
@@ -0,0 +1,69 @@
+import React from "react";
+import "./AboutStyling.css";
+
+const About = () => {
+	return (
+		<div>
+			<div className="aboutContent">
+				<div className="aboutLeftContent">
+					<h1>Pic 1</h1>
+					<h1>Video link</h1>
+				</div>
+				<div className="aboutRightContent">
+					<p>
+						The reason for writing my first book – “Tell Me a Story” came from
+						the need to leave something behind for my grandchildren. I wanted
+						them to know the person that I am – to have some knowledge of how
+						their ‘Nanny’ lived as a young child in England and how it differs
+						from their own childhood. Growing up in England and living in the
+						small Fenland village of Leverington for eighteen years of my life
+						created for me a myriad of memories – some sad, but most of them
+						very happy. I was born in the Fenland capital of Wisbech,
+						Cambridgeshire, and lived in a house that my father built. It was
+						there that I enjoyed an idyllic childhood within the safe and warm
+						embrace of my loving family, good friends and a large and lovable
+						extended family. Tell Me A Story… is a selection of stories drawn
+						from my childhood memories – from my grandmother’s infamous jelly
+						cubes to garden forks that go awry. From donkey rides to teenage
+						crushes and a drummer boy that would make you smile. Set between the
+						mid 1950s to 1970s, this memoir would appeal to adults and young
+						adults alike; particularly those familiar with the Fenland district.
+					</p>
+				</div>
+			</div>
+			<div className="aboutContent">
+				<div className="aboutLeftContent">
+					<h1>Pic 2 with sub heading</h1>
+				</div>
+				<div className="aboutRightContent">
+					<p>
+						I was born in the market town of Wisbech, Cambridgeshire, England in
+						1953. My parents had met during the war in Egypt where my mother
+						lived. When the war was over and after a lengthy engagement, they
+						married and settled in the village of Leverington, near Wisbech,
+						which is where my brother and myself grew up. My childhood, growing
+						up in the Fenland district of England, was packed with fun and
+						adventure, and yet at the same time, we were surrounded by the
+						warmth and security of our loving parents. We always knew there was
+						a safe haven to come home to at the end of each day. In September
+						1972, at the age of eighteen, I left the security of my family in
+						search of more adventure, and emigrated to Sydney, Australia, where
+						I have lived now for 43 years. I have always had a love for writing
+						and my plan, upon arriving in Australia, was to pursue a career in
+						journalism. However, this was not to be – instead I followed the
+						path of secretarial work for many years before marrying and having
+						three sons.When my youngest son started school I went back into the
+						workforce as a Teacher’s Aide (Special Needs) and stayed in that
+						line of work for 13 years.The transition from being a Teacher’s aide
+						in the Catholic School System to becoming a Pastoral Associate in
+						the Catholic Church, which is where I have been employed for the
+						past 12 years, was relatively smooth – it was a position I had been
+						preparing for over the years, and I enjoy it immensely.
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default About;
--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -4,21 +4,29 @@ import Footer from "../Footer/Footer";
 import TopNav from "../TopNav/TopNav";
 import BearBook from "../../Images/BearBook.png";
 import StoryBook from "../../Images/TellMeAStory.png";
+import About from "../About/About";
 
 const HomePage = () => {
 	return (
 		<div className="mainBodyBack">
 			<div className="mainBodyContainter">
 				<TopNav />
+
+				{/* revise top content */}
 				<div className="mainContent">
 					<img src={BearBook} alt="bear" />
+
+					{/* Revise itemlist content, try not to spam h1 tags */}
 					<div className="itemList">
 						<h1>Madeleine Pizzuti</h1>
-						<h1>hello</h1>
-						<h1>hello</h1>
+						<h1>Author of</h1>
+						<h1>'TELL ME A STORY'</h1>
+						<h1>and</h1>
+						<h1>'BEAR'</h1>
 					</div>
 					<img src={StoryBook} alt="Tellmeastory" />
 				</div>
+				<About />
 
 				<Footer />
 			</div>
